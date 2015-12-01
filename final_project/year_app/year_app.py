262a5973dbd8fb552d072ccee9ec3d893c29b55e
ericmnelson <ericmnelson.13@gmail.com>
2015-11-30 23:00:09 -0800

    Updated Web App

--- final_project/year_app/year_app.py
+++ final_project/year_app/year_app.py
@@ -18,7 +18,12 @@ import json
 app = Flask(__name__)
 
 with open('cats', 'r') as open_file:
+# with open('../../../rf.pkl', 'r') as open_file:
   model = pickle.load(open_file)
+print "done"
+
+key_dict={0:"C", 1:"C#", 2:"D", 3:"E"+u"\u266D", 4:"E", 5:"F", 6:"F#", 7:"G", 8:"A"+u"\u266D", 9:"A", 10:"B"+u"\u266D", 11:"B"}
+
 
 # 'duration', 'end_of_fade_in', 'key','key_confidence','loudness',
 # 'start_of_fade_out','tempo','time_signature''time_signature_confidence'
@@ -52,6 +57,18 @@ def get_features(song):
   viz_dict['tempo_rate'] = 60.0/float( a_sum['tempo'])
   viz_dict['loudness'] = a_sum['loudness']
   viz_dict['energy'] = a_sum['energy']*100
+  viz_dict["key"] = key_dict[a_sum['key']]
+  viz_dict["inst"] = a_sum["instrumentalness"]
+  viz_dict["liveness"] = a_sum["liveness"]
+  viz_dict["acousticness"] = a_sum["acousticness"]
+  viz_dict["valence"] = a_sum["valence"]
+  viz_dict["speech"] = a_sum["speechiness"]
+  viz_dict["artist_hottness"] = song.artist_hotttnesss
+  viz_dict["song_hotttnesss"] = song.song_hotttnesss
+  viz_dict["song_currency"] = song.get_song_currency()
+  viz_dict["artist_familiarity"] = song.get_artist_familiarity()
+  viz_dict["song_discovery"] = song.get_song_discovery()
+  pp.pprint(a_sum)
  
   pp.pprint(viz_dict)
 
@@ -66,7 +83,7 @@ def guess_song():
   count = 0
   while count < 3:
     try:
-      songs = pysong.search(title=request.form['song'], buckets=['id:7digital-US', 'tracks'], limit=True, results=50, sort="song_hotttnesss-desc")
+      songs = pysong.search(title=request.form['song'], buckets=['id:7digital-US', 'tracks', 'song_discovery'], limit=True, results=50, sort="song_hotttnesss-desc")
       break
     except:
       count+=1
@@ -82,6 +99,10 @@ def guess_song():
   # print foreign_id
   response = py7D.request('track', 'details', trackID=str(foreign_id), pageSize=3)
   rel_year = response['response']['track']['release']['releaseDate'].split("-")[0]
+  listen = py7D.preview_url(foreign_id)
+  print
+  print listen
+  print
   # pp.pprint(response['response']['track'])
   a_sum = song.audio_summary
   duration = a_sum['duration']
@@ -91,7 +112,11 @@ def guess_song():
   print year, rel_year
   return render_template('index.html', song_name=songs[0].title, predicted_year=year, artist=artist_x, 
       actual_year=rel_year, img_url=cover_url, dancy=viz_dict['dancy'], energy=viz_dict['energy'], 
-      loudness=viz_dict['loudness'], tempo=viz_dict['tempo'], tempo_rate=viz_dict['tempo_rate'])
+      loudness=viz_dict['loudness'], tempo=viz_dict['tempo'], tempo_rate=viz_dict['tempo_rate'],
+      key=viz_dict['key'], a_hot=viz_dict["artist_hottness"], s_hot=viz_dict["song_hotttnesss"],
+      a_fam = viz_dict["artist_familiarity"], song_currency = viz_dict["song_currency"], valence=viz_dict["valence"],
+      speech = viz_dict["speech"], acousticness=viz_dict["acousticness"], liveness=viz_dict["liveness"],
+      song_disc=viz_dict["song_discovery"], mp3=listen )
 
 
 if __name__ == '__main__':
