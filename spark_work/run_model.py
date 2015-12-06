06c550e29c6c753bb324b88d387b86e0bc7c5d88
Max Wolffe <max.alan.wolffe@gmail.com>
2015-12-05 17:51:27 -0800

    Add test data to run model.py

--- spark_work/run_model.py
+++ spark_work/run_model.py
@@ -5,6 +5,21 @@ from pyspark.mllib.util import MLUtils
 
 sc = SparkContext("local", "Amadeus Random Forest Run", pyFiles=['run_model.py'])
 
+data = sc.textFile("data_timbre.csv")
+zipped_data = data.zipWithIndex()
+keyed_data = zipped_data.map(lambda line: (line[-1], line[:-1]))
+
+target = sc.textFile("target_timbre.csv")
+zipped_target = target.zipWithIndex()
+keyed_target = zipped_target.map(lambda line: (line[-1], line[:-1]))
+
+target_data = keyed_data.join(keyed_target)
+labled_point_data = target_data.map(lambda tup: LabeledPoint(tup[1][1][0], tup[1][0][0].split(',')))
+
+# Split the data into training and test sets (30% held out for testing)
+print("Creating Training and Test Data Split")
+(trainingData, testData) = labled_point_data.randomSplit([0.7, 0.3])
+
 model = RandomForestModel.load(sc, "myRFModel")
 
 predictions = model.predict(testData.map(lambda x: x.features))
